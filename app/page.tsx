6ea67fc68693da6d36f75114495d1e00ae04832f
Sam Li <weisen.li@hotmail.com>
2026-05-30 20:23:59 +0000

    fix with suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-'use client';
+import { Suspense } from 'react';
 import LoginModal from './(website)/components/LoginModal';
 
 export default function HomePage() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <LoginModal showCloseButton={false} />
+      <Suspense fallback={<div className="text-gray-500">Loading...</div>}>
+        <LoginModal showCloseButton={false} />
+      </Suspense>
     </div>
   );
 }
